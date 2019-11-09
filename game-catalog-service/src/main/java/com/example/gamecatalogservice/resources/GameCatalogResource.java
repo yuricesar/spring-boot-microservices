97eeadcd628594f8a8e4ce9bc14765ed695d4c91
Yuri Oliveira <yuri.pcesar@gmail.com>
2019-11-08 22:24:45 -0300

    Utilizando WebClient do Spring Reactive no lugar de RestTemplate.

--- game-catalog-service/src/main/java/com/example/gamecatalogservice/resources/GameCatalogResource.java
+++ game-catalog-service/src/main/java/com/example/gamecatalogservice/resources/GameCatalogResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.example.gamecatalogservice.models.CatalogItem;
 import com.example.gamecatalogservice.models.Game;
@@ -21,6 +22,9 @@ public class GameCatalogResource {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 		List<Rating> ratings = Arrays.asList(
@@ -29,7 +33,15 @@ public class GameCatalogResource {
 		);
 		
 		return ratings.stream().map(rating -> {
-			Game game = restTemplate.getForObject("http://localhost:8082/games/" + rating.getGameId(), Game.class);
+			//Game game = restTemplate.getForObject("http://localhost:8082/games/" + rating.getGameId(), Game.class);
+			
+			Game game = webClientBuilder.build()
+					.get()
+					.uri("http://localhost:8082/games/" + rating.getGameId())
+					.retrieve()
+					.bodyToMono(Game.class)
+					.block();
+			
 			return new CatalogItem(game.getName(), "Desc", rating.getRating());
 		}).collect(Collectors.toList());
 
